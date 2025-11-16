24cb977a4c333972b935e28ce7a15e634fc4a364
Sergio Marquez <sergio@salire.no>
2025-11-16 12:17:08 +0100

    Add location

--- components/LocationsSection.tsx
+++ components/LocationsSection.tsx
@@ -95,6 +95,7 @@ export function LocationsSection() {
           type: "entrepreneurs",
           status: 'planned',
           time: "18:00",
+          location: "Reverb Mallorca",
           price: { amount: 0, currency: 'EUR' },
         },
       ],
